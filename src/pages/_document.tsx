6e68b3591627c69e7aa956d2e3f8a405c4b96f8c
DiDi168 <divineoko2018@gmail.com>
2024-04-27 18:43:37 +0100

    jfjfjfjf

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Home from "../pages/index"
 
 export default function Document() {
   return (
@@ -13,6 +14,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
+        <Home />
         <NextScript />
       </body>
     </Html>
