78ccadd259a603cbf85c808ccc2c57564e4dabda
DiDi168 <divineoko2018@gmail.com>
2024-04-27 20:19:49 +0100

    cqnioenoqenocqenc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,10 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="theme-color" content="#620063" />
           <meta property="og:title" content="PhilTech Academy" />
-          <meta property="og:description" content="Aquire Digital Skills with PhilTech" />
-          <meta property="og:image" content="../../public/favicon.ico" />
-          <meta property="og:url" content="" />
+          <meta property="og:description" content="Acquire Digital Skills with PhilTech" />
+          {/* Update the content attribute to point to the correct URL of the image */}
+          <meta property="og:image" content="https://example.com/path/to/image.jpg" />
+          <meta property="og:url" content="https://example.com" />
           <meta property="og:type" content="website" />
         </Head>
         <body>
