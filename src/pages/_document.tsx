92b870ad0d110d141ac3339917ff4084cfb42c3c
DiDi168 <divineoko2018@gmail.com>
2024-04-27 18:48:42 +0100

    jjffjfjfjfj

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Home from "../pages/index"
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <meta name="theme-color" content="#620063" />
-        <meta property="og:title" content="PhilTech Academy" />
-        <meta property="og:description" content="Aquire Digital Skills with PhilTech" />
-        <meta property="og:image" content="../../public/favicon.ico" />
-        <meta property="og:url" content="" />
-        <meta property="og:type" content="website" />
-      </Head>
-      <body>
-        <Main />
-        <Home />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="theme-color" content="#620063" />
+          <meta property="og:title" content="PhilTech Academy" />
+          <meta property="og:description" content="Aquire Digital Skills with PhilTech" />
+          <meta property="og:image" content="../../public/favicon.ico" />
+          <meta property="og:url" content="" />
+          <meta property="og:type" content="website" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
