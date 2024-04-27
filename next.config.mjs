ac7e5433425612977ad18c9865c122bbe6d22ced
DiDi168 <divineoko2018@gmail.com>
2024-04-27 23:13:25 +0100

    ononononon

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
-// next.config.mjs
-
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true
-  }
-
-  
 };
 
 export default nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,49 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { DocumentContext } from 'next/dist/shared/lib/utils';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head >
-     
-      <meta property="og:title" content="PhilTech Academy" />
-  <meta property="og:description" content="Aquire Digital Skills with PhilTech" />
-  <meta property="og:image" content="../../public/favicon.ico" />
-  <meta property="og:url" content="" />
-  <meta property="og:type" content="website" />
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-       </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Your meta tags and other head elements */}
+          <meta property="og:title" content="PhilTech Academy" />
+          <meta property="og:description" content="Aquire Digital Skills with PhilTech" />
+          <meta property="og:image" content="/favicon.ico" /> {/* Assuming favicon.ico is in the public folder */}
+          <meta property="og:url" content="" />
+          <meta property="og:type" content="website" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
