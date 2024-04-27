2ece9c6feef21c7da2be3ca57d6cd3d6bd331ec7
DiDi168 <divineoko2018@gmail.com>
2024-04-27 20:23:06 +0100

    work oooo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta name="theme-color" content="#620063" />
-          <meta property="og:title" content="PhilTech Academy" />
-          <meta property="og:description" content="Acquire Digital Skills with PhilTech" />
-          {/* Update the content attribute to point to the correct URL of the image */}
-          <meta property="og:image" content="https://example.com/path/to/image.jpg" />
-          <meta property="og:url" content="https://example.com" />
-          <meta property="og:type" content="website" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head >
+           <meta property="og:title" content="PhilTech Academy" />
+  <meta property="og:description" content="Aquire Digital Skills with PhilTech" />
+  <meta property="og:image" content="../../public/favicon.ico" />
+  <meta property="og:url" content="" />
+  <meta property="og:type" content="website" />
+
+       </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
