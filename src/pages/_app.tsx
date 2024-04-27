becc2d0eedbf7f5820082558559933f11056d458
DiDi168 <divineoko2018@gmail.com>
2024-04-27 23:51:32 +0100

    ovorjooejojojofjo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import Image from 'next/image';
 import '../styles/index.css'; // Import any global font styles
 
+import bro from "../assets/img/codingBro.png";
+import brain from "../assets/img/brain.png";
+import portrait from "../assets/img/manPortrait.jpg";
+import faq from '../assets/img/faq.png'
 // Define your theme object
 const theme = {
   // Add your theme properties here
