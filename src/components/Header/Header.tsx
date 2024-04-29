2ab219b4d2d07de6f566aaa4a982b7b014bc0f54
DiDi168 <divineoko2018@gmail.com>
2024-04-29 20:12:56 +0100

    kkkdkdkdk

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import  { Main, Section, Nav, MobileHeader} from './Header.styled';
-import logo from '../../assets/img/logo.png';
+import logo from '@/img/logo.png';
 
 interface ListItem {
   content: string;
