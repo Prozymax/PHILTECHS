f91444e8d59b42d7e7b1d2d25ba89668238ba310
Prozymax <maxwellprosper76@gmail.com>
2024-05-09 15:01:42 +0100

    returned again to routes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const http = require('http');
 const server = http.createServer(app);
 const pool = require('./config/db');
-// const indexRouter = require('./routes');
+const indexRouter = require('./routes');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const port = process.env.PORT;
@@ -21,37 +21,7 @@ pool.getConnection((err, connection) => {
     }
 })
 
-// app.use('/', indexRouter)
-app.get('/enroll', (request, response) => {
-    response.sendFile(__dirname + '/public/registration.html')
-})
-
-app.post('/signup', (request, response) => {
-    const { userFullName, userEmail, userTel, twitter_username, insta_username } = request.body;
-    console.log(request.body.fullname)
-    if (request.body) {
-        const insertQuery = 'INSERT INTO users (fullname, email, phone, twitter_username, instagram_username) VALUES (?, ?, ?, ?, ?)'
-        pool.query(insertQuery, [userFullName, userEmail, userTel, twitter_username, insta_username], (err, results, fields) => {
-            if (err) {
-                console.log('Err inserting data', err)
-            }
-            console.log('Data Inserted succesfully', results);
-            response.redirect('/success')
-        })
-    }
-})
-
-app.get('/success', (request, response) => {
-    response.sendFile(__dirname + '/public/success.html')
-})
-
-app.get('/contact', (request, response) => {
-    response.sendFile(__dirname + '/public/contact.html')
-})
-
-app.get('/company', (request, response) => {
-    response.sendFile(__dirname + '/public/company.html')
-})
+app.use('/', indexRouter)
 
 
 server.listen(port, () => {
