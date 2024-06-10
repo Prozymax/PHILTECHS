bc6a97033d5ab1b49146ff6bd6fa086a2aebbbe4
Prozymax <maxwellprosper76@gmail.com>
2024-06-10 16:58:56 +0100

    all changes done

--- routes/index.js
+++ routes/index.js
@@ -9,19 +9,19 @@ const indexRouter = express.Router();
 indexRouter.use(bodyParser.urlencoded({ extended: true }));
 
 // Route to serve the registration HTML page
-indexRouter.get('/Enroll', (request, response) => {
+indexRouter.get('/enroll', (request, response) => {
     response.sendFile(path.join(__dirname, '../public/registration.html'));
 });
 
 // Route to handle user registration
 indexRouter.post('/signUp', (request, response) => {
-    const { userFullName, userEmail, userTel, twitter_username, insta_username } = request.body;
+    const { userFullName, userEmail, userTel, insta_username } = request.body;
     if (userFullName && userEmail && userTel) {
-        const insertQuery = 'INSERT INTO users (fullname, email, phone, twitter_username, instagram_username) VALUES (?, ?, ?, ?, ?)';
-        pool.query(insertQuery, [userFullName, userEmail, userTel, twitter_username, insta_username], (err, results) => {
+        const insertQuery = 'INSERT INTO users (fullname, email, phone, instagram_username) VALUES (?, ?, ?, ?)';
+        pool.query(insertQuery, [userFullName, userEmail, userTel, insta_username], (err, results) => {
             if (err) {
                 console.error('Error inserting data:', err);
-                return response.status(500).send('Error inserting data');
+                return response.status(500).send('Please reload the page and try again');
             }
             console.log('Data inserted successfully:', results);
             response.redirect('/success');
